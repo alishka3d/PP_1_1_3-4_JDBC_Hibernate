517aea00bd52da374c38b5bb9466b330d15f7c9d
alishka3d <alishka3d@yandex.ru>
2025-04-02 23:29:19 +0300

    feat: add Hibernate configuration in Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,10 +11,13 @@ import java.sql.SQLException;
 public class Util {
 
     private static final Connection connection;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "999138";
+    private static final String DIALECT = "org.hibernate.dialect.PostgreSQLDialect";
+    private static final String DRIVER = "org.postgresql.Driver";
 
     static {
         try {
@@ -23,6 +30,33 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+
+            configuration.setProperty("hibernate.dialect", DIALECT);
+            configuration.setProperty("hibernate.connection.driver_class", DRIVER);
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USER);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.show_sql", "true");
+
+            configuration.addAnnotatedClass(User.class);
+
+            return configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
     }
 
     public static Connection getConnection() {
